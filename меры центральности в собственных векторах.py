1be8c3bda96b9116fde2caa74b39b5655755b53c
luna903 <228466207@qq.com>
2025-11-23 18:57:25 +0700

    Update меры центральности в собственных векторах.py

--- меры центральности в собственных векторах.py
+++ меры центральности в собственных векторах.py
@@ -15,7 +15,7 @@ for i in DC.keys():
 import networkx as nx
 import matplotlib.pyplot as plt
 G = nx.Graph()
-G.add_edges_from([ (1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6),(5, 6)])
+G.add_edges_from([(1,2), (1,3), (1,4), (2,3), (2,4), (3,4), (3,5), (4,6),(5,6)])
 nx.draw(G, node_size = 500, with_labels=True)
 plt.show()
 print(nx.degree_centrality(G))
@@ -23,4 +23,5 @@ DC = nx.degree_centrality(G)
 for i in DC.keys():
     print('The node {} measurement center value {:.4f}'.format(i, DC[i]))
     print(nx.eigenvector_centrality(G))#特征向量的中心性
+
 
